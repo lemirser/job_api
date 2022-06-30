2103f58e25b789dbcaab266bd4156603945c6e03
Leandro <serzoleandro@gmail.com>
2022-06-30 22:06:21 +0800

    Setup function for fetching job post

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,24 @@
 from bs4 import BeautifulSoup
 from flask import Blueprint, jsonify, request
+import requests
 
-jobs_post = Blueprint("jobs", __name__, url_prefix="/api/v1/jobs")
+# job_post = Blueprint("jobs", __name__, url_prefix="/api/v1/jobs")
+
+
+# @job_post.get("/fetch")
+def fetch_job():
+    job_position = "data+engineer"
+    html_text = requests.get(
+        # .text will not display the RESPONSE code from the website
+        f"https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords={job_position}&txtLocation="
+    ).text
+
+    soup = BeautifulSoup(html_text, "lxml")
+
+    job = soup.find("li", class_="clearfix job-bx wht-shd-bx")
+
+    print(job.text)
+
+
+if __name__ == "__main__":
+    fetch_job()
