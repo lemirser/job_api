298449a0c4aafe7cc513d551c4485ba3de980da0
Leandro <serzoleandro@gmail.com>
2022-07-04 21:51:46 +0800

    Fetch the user input from job_post input

--- src/home.py
+++ src/home.py
@@ -1,8 +1,21 @@
-from flask import Blueprint, Flask, render_template
+from flask import Blueprint, Flask, render_template, request
 
 home = Blueprint("home", __name__, url_prefix="/")
 
 
 @home.get("/")
 def home_page():
+
+    return render_template("home.html")
+
+
+@home.route("/", methods=["POST", "GET"])
+def fetch_input():
+    """
+    Retrieve the data from the job_post (name) input.
+    """
+    if request.method == "POST":
+        default_value = 0
+        data = request.form.get("job_post", default_value)
+        print(f"Data: {data} | Type: {type(data)}")
     return render_template("home.html")
